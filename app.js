99d52755bb9210d1124bebbc8e5883f9d1ea4e6f
selftuts <selftuts@gmail.com>
2017-08-19 11:58:43 +0000

    creating get route in express

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.set('secretKey','12345');
 var secretKey = app.get('secretKey');
 
 
+// create get endpoint
+app.get('/user',function(req,res){
+
+    res.json({"name":"Hi Selftuts"});
+
+});
+
+
 
 // creating an express http server
 app.listen(3000,function(){
